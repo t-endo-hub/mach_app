430d46112a9a90558d0ad551872087f343298261
Takumi Endo <endotaku0608@gmail.com>
2020-08-15 11:51:10 +0000

    [Add]reaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_004636) do
+ActiveRecord::Schema.define(version: 2020_08_15_095553) do
+
+  create_table "reactions", force: :cascade do |t|
+    t.integer "to_user_id", null: false
+    t.integer "from_user_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
