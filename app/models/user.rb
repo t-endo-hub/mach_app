fecb7906a337984df2c72cb869729cd82967f9b1
Takumi Endo <endotaku0608@gmail.com>
2020-08-16 01:21:32 +0000

    [Add]chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :reactions
+  has_many :chat_messages
+  has_many :chat_room_users
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
